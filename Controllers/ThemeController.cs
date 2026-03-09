bfcb81be97b0b8beda487cf55293a822ac3be1f2
sofien <sofiensghaier2@gmail.com>
2026-03-09 13:11:50 +0100

    last step

---
+++ Controllers/ThemeController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EventFinder.Controllers
+{
+    public class ThemeController : Controller
+    {
+        // Save theme preference in session or cookie
+        [HttpPost]
+        public IActionResult SetTheme(string theme)
+        {
+            // Store in cookie (lasts 1 year)
+            CookieOptions option = new CookieOptions
+            {
+                Expires = DateTime.Now.AddYears(1),
+                HttpOnly = true,
+                SameSite = SameSiteMode.Lax
+            };
+
+            Response.Cookies.Append("UserTheme", theme, option);
+
+            return Json(new { success = true, theme = theme });
+        }
+
+        // Get current theme
+        [HttpGet]
+        public IActionResult GetTheme()
+        {
+            var theme = Request.Cookies["UserTheme"] ?? "light";
+            return Json(new { theme = theme });
+        }
+    }
+}
